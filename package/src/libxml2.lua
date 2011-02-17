642105ee39de31cba4ec92a2becb3d543ec2a6da
Tetsurou Shindou <te_shindou@timedia.co.jp>
2011-02-17 19:56:22 +0900

    add libxml2

--- package/src/libxml2.lua
+++ package/src/libxml2.lua
@@ -1,17 +1,15 @@
 srcPackage {
-    name="fontconfig",
-    version="2.8.0",
+    name="libxml2",
+    version="2.7.8",
     url={
-        "http://www.freedesktop.org/software/fontconfig/release/fontconfig-2.8.0.tar.gz"
+        "ftp://xmlsoft.org/libxml2/libxml2-sources-2.7.8.tar.gz"
     },
-    depends={
-        "freetype", "libxml",
-    },
+ 
     sh=[=[
 set -x
 mkdir -p $CCS_TARGET_ROOT/src
 cd $CCS_TARGET_ROOT
-archive=fontconfig-2.8.0.tar.gz
+archive=libxml2-sources-2.7.8.tar.gz
 archive_dir=`extract $CCS_DOWNLOAD/$archive src`
 cd src/$archive_dir
 ./configure --host=$CCS_TARGET --prefix=$CCS_TARGET_ROOT
