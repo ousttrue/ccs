5d29e2b00a66302dee00a4e2680c2705709e6bf0
U-theremin\focke <ousttrue@gmail.com>
2011-03-01 08:37:57 +0900

    fix

---
+++ package/src/Python.lua
@@ -0,0 +1,19 @@
+srcPackage {
+    name="Python",
+    version="3.2",
+    url={
+        "http://www.python.org/ftp/python/3.2/Python-3.2.tar.bz2"
+    },
+ 
+    sh=[=[
+set -x
+export CFLAGS="$CFLAGS -DMS_WINDOWS"
+mkdir -p $CCS_TARGET_ROOT/src
+cd $CCS_TARGET_ROOT
+archive=Python-3.2.tar.bz2
+archive_dir=`extract $CCS_DOWNLOAD/$archive src`
+cd src/$archive_dir
+./configure --host=$CCS_TARGET --prefix=$CCS_TARGET_ROOT
+make install -j4
+    ]=],
+}
