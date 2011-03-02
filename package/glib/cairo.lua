75c2d804f2d615248a6f66d58641142f2857eeec
U-theremin\focke <ousttrue@gmail.com>
2011-03-02 20:21:59 +0900

    fix

--- package/glib/cairo.lua
+++ package/glib/cairo.lua
@@ -10,6 +10,8 @@ srcPackage {
     },
     sh=[=[
 set -x
+echo $PKG_CONFIG_PATH
+pkg-config --cflags libpng
 export CFLAGS="$CFLAGS `pkg-config --cflags glib-2.0`"
 export LDFLAGS="$LDFLAGS `pkg-config --libs glib-2.0`"
 mkdir -p $CCS_TARGET_ROOT/src
@@ -18,7 +20,7 @@ archive=cairo-1.10.2.tar.gz
 archive_dir=`extract $CCS_DOWNLOAD/$archive src`
 cd src/$archive_dir
 ./configure --host=$CCS_TARGET --prefix=$CCS_TARGET_ROOT --enable-gl --enable-wgl || exit 1
-(cd src && patch < $CCS_ROOT/package-gtk+/cairo.ffs.patch)
+(cd src && patch < $CCS_ROOT/package/glib/cairo.ffs.patch)
 fixlibtool
 make -j4 install
     ]=],
--- package/glib/gtk+.lua
+++ package/glib/gtk+.lua
@@ -18,9 +18,14 @@ cd $CCS_TARGET_ROOT
 archive=gtk+-3.0.0.tar.bz2
 archive_dir=`extract $CCS_DOWNLOAD/$archive src`
 cd src/$archive_dir
-GLIB_COMPILE_SCHEMAS=$CCS_TARGET_ROOT/bin/glib-compile-schemas.exe ./configure --host=$CCS_TARGET --build=i686-pc-cygwin --prefix=$CCS_TARGET_ROOT || exit 1
+GLIB_COMPILE_SCHEMAS=$CCS_TARGET_ROOT/bin/glib-compile-schemas.exe ./configure --host=$CCS_TARGET --build=i686-pc-cygwin --prefix=$CCS_TARGET_ROOT --without-x || exit 1
 fixglib
 fixlibtool
 make install -j4
+pushd gtk
+make gtk-update-icon-cache.exe
+patch < $CCS_ROOT/package/glib/gtk+.Makefile.patch
+popd
+make install
     ]=],
 }
