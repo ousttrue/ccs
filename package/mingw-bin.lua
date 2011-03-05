83d12f1f5a03d70a3358587a675774c3d4c62daf
U-theremin\focke <ousttrue@gmail.com>
2011-03-05 15:25:38 +0900

    add i686-pc-mingw32 binary pthreads

--- package/mingw-bin.lua
+++ package/mingw-bin.lua
@@ -7,3 +7,14 @@ binaryPackage {
         "http://jaist.dl.sourceforge.net/project/mingw/MinGW/libtool/libtool-2.4-1/libtool-2.4-1-mingw32-bin.tar.lzma",
     },
 }
+binaryPackage {
+    name="pthreads",
+    version="2.8.0.3",
+    release="2",
+    host="i686-pc-mingw32",
+    url={
+        "http://jaist.dl.sourceforge.net/project/mingw/MinGW/pthreads-w32/pthreads-w32-2.8.0-3/libpthread-2.8.0-3-mingw32-dll-2.tar.lzma",
+        "http://jaist.dl.sourceforge.net/project/mingw/MinGW/pthreads-w32/pthreads-w32-2.8.0-3/pthreads-w32-2.8.0-3-mingw32-dev.tar.lzma",
+    },
+}
+
