6f5aa9d5dd48a0142e4cf780afa10ac50d6edd4e
ousttrue <ousttrue@gmail.com>
2011-05-03 03:18:04 +0900

    fix pcre

--- package/local/pcre.lua
+++ package/local/pcre.lua
@@ -12,6 +12,7 @@ archive=pcre-8.12.tar.bz2
 archive_dir=`extract $CCS_DOWNLOAD/$archive src`
 cd src/$archive_dir
 ./configure --host=$CCS_TARGET --prefix=$CCS_TARGET_ROOT || exit 1
-make install -j4
+make -j4 || exit 1
+make install
     ]=],
 }
