def92314a14973401d1b1e45123f25d07114b06f
ousttrue <ousttrue@gmail.com>
2011-05-22 14:22:29 +0900

    fix python deprecated api in swig2

--- package/local/swig.lua
+++ package/local/swig.lua
@@ -1,8 +1,8 @@
 srcPackage {
     name="swig",
-    version="2.0.2",
+    version="2.0.3",
     url={
-        "http://jaist.dl.sourceforge.net/project/swig/swig/swig-2.0.2/swig-2.0.2.tar.gz"
+        "http://jaist.dl.sourceforge.net/project/swig/swig/swig-2.0.3/swig-2.0.3.tar.gz"
     },
     depends={
         "pcre",
@@ -11,9 +11,11 @@ srcPackage {
 set -x
 mkdir -p $CCS_TARGET_ROOT/src
 cd $CCS_TARGET_ROOT
-archive=swig-2.0.2.tar.gz
+archive=swig-2.0.3.tar.gz
 archive_dir=`extract $CCS_DOWNLOAD/$archive src`
 cd src/$archive_dir
+# swig200-rh623854.patch
+(cd Lib/python && patch < $CCS_SELF_DIR/swig-2.0.3-pyrun.swg.diff)
 ./configure --host=$CCS_TARGET --prefix=$CCS_TARGET_ROOT || exit 1
 make -j4 || exit 1
 make install
