40eeab3b23e4cdcfc3045299f458b7b0cbd0706d
ousttrue <ousttrue@gmail.com>
2011-06-02 08:26:26 +0900

    fix gettext

--- package/local/gettext.lua
+++ package/local/gettext.lua
@@ -49,7 +49,23 @@ cd gettext-runtime
 ./configure --host=$CCS_TARGET --prefix=$CCS_TARGET_ROOT --without-emacs --with-ncludeed-gettext --without-csharp --disable-libasprintf
 fixlibtool
 find gettext-tools -name "*.h" | xargs perl -i.bak -pe 's/DLL_VARIABLE//g'
-make install -j4
+perl -i.bak -pe 's/(LIBS = .*)/\1 -lpthread' intl/Makefile
+cat <<_PATCH_ | (cd tests; patch)
+--- test-lock.c.orig	2011-06-02 08:22:00 +0900
++++ test-lock.c	2011-06-02 08:23:52 +0900
+@@ -106,7 +106,8 @@
+ }
+ static inline void * gl_thread_self (void)
+ {
+-  return (void *) pthread_self ();
++  pthread_t tmp=pthread_self();
++  return (void *) *((void**)&tmp);
+ }
+ #endif
+ #if TEST_PTH_THREADS
+_PATCH_
+make -j4 || exit 1
+make install
     ]=],
 }
 --]]
