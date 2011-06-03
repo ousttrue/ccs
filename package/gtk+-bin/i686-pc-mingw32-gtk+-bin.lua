d60d23854a33727ed27d0952371b4fac3994147f
ousttrue <ousttrue@gmail.com>
2011-06-03 22:00:38 +0900

    fix

--- package/gtk+-bin/i686-pc-mingw32-gtk+-bin.lua
+++
@@ -1,9 +0,0 @@
-binaryPackage {
-    name="gtk+",
-    version="2.22.1",
-    revision="20101227",
-    host="i686-pc-mingw32",
-    url={
-        "http://ftp.gnome.org/pub/gnome/binaries/win32/gtk+/2.22/gtk+-bundle_2.22.1-20101227_win32.zip",
-    },
-}
