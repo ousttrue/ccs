d8db73ee4cad7831afe27e7ee9780d07dd2fff5a
ousttrue <ousttrue@gmail.com>
2011-06-05 13:43:14 +0900

    add gcc-4.5.2-tdm-sjlj

---
+++ toolchain/i686-pc-mingw32-gcc-4.5.2-tdm-sjlj.lua
@@ -0,0 +1,28 @@
+------------------------------------------------------------------------------
+-- tdm: http://tdm-gcc.tdragon.net/
+------------------------------------------------------------------------------
+toolchainPackage {
+    name="depends",
+    target="i686-pc-mingw32",
+    depends={
+        {"binutils", "msys", "i686-pc-mingw32"},
+        {"mingwrt", "i686-pc-mingw32"},
+        {"w32api", "i686-pc-mingw32"},
+    },
+}
+toolchainPackage {
+    name="gcc",
+    target="i686-pc-mingw32",
+    version="4.5.2",
+    release="1",
+    url={
+        "http://jaist.dl.sourceforge.net/project/tdm-gcc/TDM-GCC%204.5%20series/4.5.2-tdm-1%20SJLJ/gcc-4.5.2-tdm-1-core.tar.lzma",
+        "http://jaist.dl.sourceforge.net/project/tdm-gcc/TDM-GCC%204.5%20series/4.5.2-tdm-1%20SJLJ/gcc-4.5.2-tdm-1-c++.tar.lzma",
+        "",
+    },
+    post=[=[
+cd $CCS_TARGET_ROOT/bin
+cp gcc.exe cc.exe
+    ]=],
+}
+
